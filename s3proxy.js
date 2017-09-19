919dc6ec2fdbdea728cde16c03c142693e1fe8a4
George Moon <gmoon@mmm.com>
2017-09-19 19:47:47 -0400

    added get method with custom httpHeaders event

--- s3proxy.js
+++ s3proxy.js
@@ -1,9 +1,24 @@
 /* jslint node: true, esversion: 6 */
 
 const EventEmitter = require('events');
+const AWS = require('aws-sdk');
 
 module.exports = class s3proxy extends EventEmitter {
-  init() {
+  init(p) {
+    const params = p || {};
+    params.apiVersion = '2006-03-01';
+    this.credentials = new AWS.SharedIniFileCredentials();
+    AWS.config.credentials = this.credentials;
+    this.s3 = new AWS.S3(params);
     this.emit('init');
+  }
+  get(bucket, key) {
+    const params = { Bucket: bucket, Key: key };
+    const s3request = this.s3.getObject(params);
+    const s3stream = s3request.createReadStream();
+    s3request.on('httpHeaders', (statusCode, headers) => {
+      s3stream.emit('httpHeaders', statusCode, headers);
+    });
+    return s3stream;
   }
 };
--- test.js
+++ test.js
@@ -4,20 +4,66 @@ const chai = require('chai');
 const S3Proxy = require('./s3proxy.js');
 
 const { expect } = chai;
-const proxy = new S3Proxy();
 
 describe('s3proxy', () => {
   describe('constructor', () => {
     it('should be an object', () => {
+      const proxy = new S3Proxy();
       expect(proxy).to.be.an('object');
     });
   });
   describe('initialization', () => {
+    const proxy = new S3Proxy();
     it("should emit an 'init' event", (done) => {
       proxy.on('init', () => {
         done();
       });
       proxy.init();
+    });
+  });
+  describe('get error codes', () => {
+    const proxy = new S3Proxy();
+    before(() => {
+      proxy.init();
+    });
+    it('should return error code NoSuchBucket for nonexistent bucket', (done) => {
+      const stream = proxy.get('.Bucket.name.cannot.start.with.a.period', 'xxxx');
+      stream.on('error', (error) => {
+        expect(error.code).to.equal('NoSuchBucket');
+        done();
+      });
+    });
+    it('should return error code NoSuchKey for nonexistent key', (done) => {
+      const stream = proxy.get('codeassist-repo', 'small.txt');
+      stream.on('error', (error) => {
+        expect(error.code).to.equal('NoSuchKey');
+        done();
+      });
+    });
+  });
+  describe('get', () => {
+    const proxy = new S3Proxy();
+    const page = {};
+    before((done) => {
+      proxy.init();
+      const stream = proxy.get('codeassist-repo', 'index.html');
+      page.length = 0;
+      stream.on('data', (chunk) => {
+        page.length += chunk.length;
+      });
+      stream.on('httpHeaders', (statusCode, headers) => {
+        page.headers = headers;
+        page.statusCode = statusCode;
+      });
+      stream.on('end', () => {
+        done();
+      });
+    });
+    it('should have headers', () => {
+      expect(page.headers).to.have.keys(['accept-ranges', 'content-length', 'content-type', 'date', 'etag', 'last-modified', 'server', 'x-amz-id-2', 'x-amz-request-id']);
+    });
+    it('should have length of 58', () => {
+      expect(page.length).to.equal(58);
     });
   });
 });
