0f7a4b32d5a62b4d4216cdd586516b38929e1d95
George Moon <george.moon@gmail.com>
2020-05-05 11:23:06 -0400

    #24 added AWS Serverless Application Model example for serving binary media ttypes using S3Proxy and API Gateway

---
+++ examples/sam-app/s3proxy/app.js
@@ -0,0 +1,15 @@
+const awsServerlessExpress = require('aws-serverless-express');
+const express = require('express');
+const S3Proxy = require('s3proxy');
+const app = express();
+const proxy = new S3Proxy({ bucket: 's3proxy-public' });
+proxy.init();
+
+app.route('/*').get(proxyToS3);
+
+function proxyToS3(req, res) {
+         proxy.get(req,res).on('error', () => res.end()).pipe(res);
+}
+
+const server = awsServerlessExpress.createServer(app, null, ['image/jpeg']);
+exports.lambdaHandler = (event, context) => awsServerlessExpress.proxy(server, event, context);
